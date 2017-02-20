270d2bfb83c9bcb7343ab0444db04c8756c9e67c
drod0303 <drod0303@yahoo.com>
2017-02-20 17:29:20 -0600

    GUI update
    daily usage reminder added

--- SlimTech/SettingsVC.swift
+++ SlimTech/SettingsVC.swift
@@ -10,9 +10,26 @@ import UIKit
 
 class SettingsVC: UIViewController {
     
+    @IBOutlet weak var timeAlertSwitch: UISwitch!
+    
+    @IBOutlet weak var timeAlertPicker: UIDatePicker!
+    
+    @IBOutlet weak var saveButton: UIButton!
+    
+    @IBOutlet weak var timeAlertLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.black
+        timeAlertPicker.setValue(UIColor.white, forKeyPath: "textColor")
+        timeAlertPicker.timeZone = NSTimeZone.local
+        
+        saveButton.isHidden = true
+        
+        timeAlertSwitch.isOn = false
+        timeAlertPicker.isHidden = true
+        
         
         
     }
@@ -34,4 +51,58 @@ class SettingsVC: UIViewController {
         dismiss(animated: true, completion: nil)
     }
 
+    @IBAction func timeAlertSwitchPressed(_ sender: Any) {
+        if(timeAlertSwitch.isOn){
+            timeAlertPicker.isHidden = false
+            saveButton.isHidden = false
+        }
+        else{
+            
+            timeAlertPicker.isHidden = true
+            saveButton.isHidden = true
+            timeAlertLabel.text = "Daily Usage Reminder"
+        }
+    }
+    
+    @IBAction func saveButtonPressed(_ sender: Any) {
+        saveButton.isHidden = true
+        if(timeAlertPicker.isHidden == false){
+            var hour = timeAlertPicker.value(forKeyPath: "hour") as? Int
+            var minute = timeAlertPicker.value(forKeyPath: "minute") as? Int
+            var minuteString = ""
+            var hourString = ""
+            
+            if (minute!<10){
+                minuteString = "0\(minute!)"
+            }
+            else{
+                minuteString = "\(minute!)"
+            }
+            
+            if (hour!>=12){
+                if (hour! == 12){
+                    hour! += 0
+                }
+                else{
+                    hour! -= 12
+                }
+                hourString = "\(hour!)"
+                timeAlertLabel.text = "Usage Reminder   " + hourString + ":" + minuteString + " PM"
+                
+            }
+            else{
+                if (hour! == 0){
+                    hour! += 12
+                }
+                hourString = "\(hour!)"
+                timeAlertLabel.text = "Usage Reminder   " + hourString + ":" + minuteString + " AM"
+            }
+            timeAlertPicker.isHidden = true
+        }
+    }
+    
+    
+    
+    
+    
 }
